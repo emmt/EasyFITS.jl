a1bb0cd2711713cf899ffd7cb8283e20a85a9aec
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2020-10-20 11:06:24 +0200

    avoid an infinite loop with something like type_from_bitpix(32)

--- src/patches.jl
+++ src/patches.jl
@@ -16,3 +16,7 @@ Base.get(hdr::FITSHeader, key::String, def) =
 
 # Should be in CFITSIO.jl/src/CFITSIO.jl
 Base.isopen(f::FITSFile) = (f.ptr != C_NULL)
+
+# Should be in CFITSIO.jl/src/CFITSIO.jl, this is to avoid an infinite loop
+# with something like type_from_bitpix(32) (because Int may not be Cint).
+CFITSIO.type_from_bitpix(code::Integer) = type_from_bitpix(Val(Cint(code)))
