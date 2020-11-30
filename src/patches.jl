6e71dfe85681384cbdea4cb8e18d6aa5f938c9a0
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2020-11-30 15:12:25 +0100

    Remove patch for type_from_bitpix

--- src/patches.jl
+++ src/patches.jl
@@ -16,9 +16,3 @@ Base.get(hdr::FITSHeader, key::String, def) =
 
 # Should be in CFITSIO.jl/src/CFITSIO.jl
 Base.isopen(f::FITSFile) = (f.ptr != C_NULL)
-
-# Should be in CFITSIO.jl/src/CFITSIO.jl, this is to avoid an infinite loop
-# with something like type_from_bitpix(32) (because Int may not be Cint).
-type_from_bitpix(code::Integer) = CFITSIO.type_from_bitpix(Val(Cint(code)))
-type_from_bitpix(code) = error("invalid non-integer bitpix code")
-CFITSIO.type_from_bitpix(::Val{X}) where {X} = error("invalid bitpix code $X")
