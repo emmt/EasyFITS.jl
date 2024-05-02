25dc28a4332d4d96ccbcbce8c4b517968fe5f8cf
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2024-05-02 15:55:13 +0200

    Patch Clang when building EasyFITS

--- deps/build.jl
+++ deps/build.jl
@@ -3,6 +3,32 @@ using Clang.Generators
 using Clang.LibClang.Clang_jll
 using CFITSIO_jll
 
+import Clang
+
+# NOTE There is a bug in Clang < 0.18.0 that may prevent building EasyFITS.
+# See https://github.com/JuliaInterop/Clang.jl/issues/450 for more informations.
+#
+# The following is a hack to patch the code.
+#
+# Other possibility to replace the rather crude test (but requires Compat):
+#
+#     import Compat
+#     if Compat.pkgversion(Clang) < v"0.18.0"
+#         ...
+#     end
+#
+if isdefined(Clang, :JLLEnvs) && isdefined(Clang.JLLEnvs, :get_system_shard_key)
+    @warn "A patched version of `Clang.JLLEnvs.get_system_shard_key` will be used for building `EasyFITS` (see https://github.com/JuliaInterop/Clang.jl/issues/450)"
+    function Clang.JLLEnvs.get_system_shard_key(triple::String)
+        @assert triple ∈ Clang.JLLEnvs.JLL_ENV_ARTIFACT_TRIPLES "Wrong JLL target triple: $triple. Please choose a triple listed in `JLL_ENV_ARTIFACT_TRIPLES`."
+        platform_keys = filter(collect(keys(Clang.JLLEnvs.JLL_ENV_SHARDS))) do key
+            startswith(key, "$(Clang.JLLEnvs.JLL_ENV_SYSTEM_SHARD_NAME)-$triple.") &&
+            endswith(key, "$(Clang.JLLEnvs.JLL_ENV_HOST_TRIPLE).unpacked")
+        end
+        return first(platform_keys)
+    end
+end
+
 # We wrap everything into a function to avoid having undefined variables...
 function build_deps()
 
